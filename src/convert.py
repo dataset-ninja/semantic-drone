ed745f15a312bc29ae2e47c3b5ada1ec0f10e91a
Ars Vladimirov <grokhael@gmail.com>
2023-09-14 13:56:20 +0600

    init settings

--- src/convert.py
+++ src/convert.py
@@ -1,73 +1,112 @@
-import supervisely as sly
-import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
-from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
-import shutil
+# https://www.tugraz.at/index.php?id=22387
 
+import csv
+import os
+import shutil
+from urllib.parse import unquote, urlparse
+
+import numpy as np
+import supervisely as sly
+from cv2 import connectedComponents
+from dataset_tools.convert import unpack_if_archive
+from dotenv import load_dotenv
+from PIL import Image
+from supervisely.io.fs import (
+    dir_exists,
+    file_exists,
+    get_file_ext,
+    get_file_name,
+    get_file_name_with_ext,
+    get_file_size,
+)
 from tqdm import tqdm
 
-def download_dataset(teamfiles_dir: str) -> str:
-    """Use it for large datasets to convert them on the instance"""
-    api = sly.Api.from_env()
-    team_id = sly.env.team_id()
-    storage_dir = sly.app.get_data_dir()
-
-    if isinstance(s.DOWNLOAD_ORIGINAL_URL, str):
-        parsed_url = urlparse(s.DOWNLOAD_ORIGINAL_URL)
-        file_name_with_ext = os.path.basename(parsed_url.path)
-        file_name_with_ext = unquote(file_name_with_ext)
-
-        sly.logger.info(f"Start unpacking archive '{file_name_with_ext}'...")
-        local_path = os.path.join(storage_dir, file_name_with_ext)
-        teamfiles_path = os.path.join(teamfiles_dir, file_name_with_ext)
-        fsize = get_file_size(local_path)
-        with tqdm(desc=f"Downloading '{file_name_with_ext}' to buffer..", total=fsize) as pbar:
-            api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
-        dataset_path = unpack_if_archive(local_path)
-
-    if isinstance(s.DOWNLOAD_ORIGINAL_URL, dict):
-        for file_name_with_ext, url in s.DOWNLOAD_ORIGINAL_URL.items():
-            local_path = os.path.join(storage_dir, file_name_with_ext)
-            teamfiles_path = os.path.join(teamfiles_dir, file_name_with_ext)
-
-            if not os.path.exists(get_file_name(local_path)):
-                fsize = get_file_size(local_path)
-                with tqdm(
-                    desc=f"Downloading '{file_name_with_ext}' to buffer {local_path}...",
-                    total=fsize,
-                    unit="B",
-                    unit_scale=True,
-                ) as pbar:
-                    api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
-
-                sly.logger.info(f"Start unpacking archive '{file_name_with_ext}'...")
-                unpack_if_archive(local_path)
-            else:
-                sly.logger.info(
-                    f"Archive '{file_name_with_ext}' was already unpacked to '{os.path.join(storage_dir, get_file_name(file_name_with_ext))}'. Skipping..."
-                )
-
-        dataset_path = storage_dir
-    return dataset_path
-
+import src.settings as s
 
 
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    # project_name = "semantic_drone_dataset"
+    dataset_path = (
+        "/mnt/d/datasetninja-raw/semantic drone dataset/semantic_drone_dataset/training_set"
+    )
+    images_folder = "images"
+    masks_folder = "gt/semantic/label_images"
+    masks_values_data_file = "gt/semantic/class_dict.csv"
+    images_ext = ".jpg"
+    masks_ext = ".png"
+    ds_name = "ds"
+    batch_size = 1
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    def get_unique_colors(img):
+        unique_colors = []
+        img = img.astype(np.int32)
+        h, w = img.shape[:2]
+        colhash = img[:, :, 0] * 256 * 256 + img[:, :, 1] * 256 + img[:, :, 2]
+        unq, unq_inv, unq_cnt = np.unique(colhash, return_inverse=True, return_counts=True)
+        indxs = np.split(np.argsort(unq_inv), np.cumsum(unq_cnt[:-1]))
+        col2indx = {unq[i]: indxs[i][0] for i in range(len(unq))}
+        for col, indx in col2indx.items():
+            if col != 0:
+                unique_colors.append((col // (256**2), (col // 256) % 256, col % 256))
 
-    # ... some code here ...
+        return unique_colors
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+    def create_ann(image_path):
+        labels = []
 
-    # return project
+        mask_path = os.path.join(masks_path, get_file_name(image_path) + masks_ext)
+        if file_exists(mask_path):
+            mask_np = sly.imaging.image.read(mask_path)
+            img_height = mask_np.shape[0]
+            img_wight = mask_np.shape[1]
+            unique_colors = get_unique_colors(mask_np)
+            for color in unique_colors:
+                mask = np.all(mask_np == color, axis=2)
+                ret, curr_mask = connectedComponents(mask.astype("uint8"), connectivity=8)
+                for i in range(1, ret):
+                    obj_mask = curr_mask == i
+                    bitmap = sly.Bitmap(data=obj_mask)
+                    if bitmap.area > 50:
+                        obj_class = color_to_obj_class[color]
+                        label = sly.Label(bitmap, obj_class)
+                        labels.append(label)
 
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
 
+    color_to_obj_class = {}
+    masks_values_data_path = os.path.join(dataset_path, masks_values_data_file)
+    with open(masks_values_data_path, "r") as file:
+        csvreader = csv.reader(file)
+        for idx, row in enumerate(csvreader):
+            if idx != 0:
+                color = tuple(map(int, row[1:]))
+                color_to_obj_class[color] = sly.ObjClass(row[0], sly.Bitmap)
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+
+    meta = sly.ProjectMeta(obj_classes=list(color_to_obj_class.values()))
+    api.project.update_meta(project.id, meta.to_json())
+
+    images_path = os.path.join(dataset_path, images_folder)
+    masks_path = os.path.join(dataset_path, masks_folder)
+
+    dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+    images_names = [im_name for im_name in os.listdir(images_path)]
+
+    progress = sly.Progress("Create dataset {}".format(ds_name), len(images_names))
+
+    for images_names_batch in sly.batched(images_names, batch_size=batch_size):
+        images_pathes_batch = [os.path.join(images_path, im_name) for im_name in images_names_batch]
+
+        img_infos = api.image.upload_paths(dataset.id, images_names_batch, images_pathes_batch)
+        img_ids = [im_info.id for im_info in img_infos]
+
+        anns = [create_ann(image_path) for image_path in images_pathes_batch]
+        api.annotation.upload_anns(img_ids, anns)
+
+        progress.iters_done_report(len(images_names_batch))
+
+    return project
--- src/settings.py
+++ src/settings.py
@@ -1,45 +1,80 @@
 from typing import Dict, List, Optional, Union
 
-from dataset_tools.templates import AnnotationType, CVTask, Industry, Domain, Research, License
+from dataset_tools.templates import (
+    AnnotationType,
+    Category,
+    CVTask,
+    Domain,
+    Industry,
+    License,
+    Research,
+)
 
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "Semantic Drone"
+PROJECT_NAME_FULL: str = "Semantic Drone Dataset v1.1"
+HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+LICENSE: License = License.Custom(url="http://dronedataset.icg.tugraz.at/", redistributable=False)
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Domain.DroneInspection()]
+CATEGORY: Category = Category.Aerial(extra=Category.Drones())
 
-RELEASE_YEAR: int = None
-HOMEPAGE_URL: str = None
+CV_TASKS: List[CVTask] = [CVTask.SemanticSegmentation()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.SemanticSegmentation()]
+
+RELEASE_DATE: Optional[str] = "2019-01-25"  # e.g. "YYYY-MM-DD"
+if RELEASE_DATE is None:
+    RELEASE_YEAR: int = None
+
+HOMEPAGE_URL: str = "https://www.tugraz.at/index.php?id=22387"
 # e.g. "https://some.com/dataset/homepage"
 
 PREVIEW_IMAGE_ID: int = None
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/semantic-drone"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[
+    Union[str, dict]
+] = "https://docs.google.com/forms/d/e/1FAIpQLSfhqqdqoB-2c3S8FVq5YsToTExF-EQ4KFeUrYvEuylGSDX0kA/viewform"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
-PAPER: Optional[str] = None
-CITATION_URL: Optional[str] = None
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
-TAGS: List[str] = None
+# If you have more than the one paper, put the most relatable link as the first element of the list
+PAPER: Optional[Union[str, List[str]]] = None
+BLOGPOST: Optional[Union[str, List[str]]] = None
+
+CITATION_URL: Optional[str] = "http://dronedataset.icg.tugraz.at/"
+AUTHORS: Optional[List[str]] = [
+    "Christian Mostegel",
+    "Michael Maurer",
+    "Nikolaus Heran",
+    "Jesus Pestana Puerta",
+    "Friedrich Fraundorfer",
+]
+
+ORGANIZATION_NAME: Optional[
+    Union[str, List[str]]
+] = "Institute of Computer Graphics and Vision (ICG), Graz University of Technology, Austria"
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = "https://www.tugraz.at/institute/icg/home"
+
+# Set '__PRETEXT__' or '__POSTTEXT__' as a key with value:str to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
+SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
+TAGS: Optional[List[str]] = None
+
+
+SECTION_EXPLORE_CUSTOM_DATASETS: Optional[List[str]] = None
 
 ##################################
 ###### ? Checks. Do not edit #####
@@ -53,10 +88,17 @@ def check_names():
 
 
 def get_settings():
+    if RELEASE_DATE is not None:
+        global RELEASE_YEAR
+        RELEASE_YEAR = int(RELEASE_DATE.split("-")[0])
+
     settings = {
         "project_name": PROJECT_NAME,
+        "project_name_full": PROJECT_NAME_FULL or PROJECT_NAME,
+        "hide_dataset": HIDE_DATASET,
         "license": LICENSE,
         "applications": APPLICATIONS,
+        "category": CATEGORY,
         "cv_tasks": CV_TASKS,
         "annotation_types": ANNOTATION_TYPES,
         "release_year": RELEASE_YEAR,
@@ -68,13 +110,18 @@ def get_settings():
     if any([field is None for field in settings.values()]):
         raise ValueError("Please fill all fields in settings.py after uploading to instance.")
 
-    settings["project_name_full"] = PROJECT_NAME_FULL or PROJECT_NAME
+    settings["release_date"] = RELEASE_DATE
     settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
     settings["class2color"] = CLASS2COLOR
     settings["paper"] = PAPER
+    settings["blog"] = BLOGPOST
     settings["citation_url"] = CITATION_URL
+    settings["authors"] = AUTHORS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
-    settings["tags"] = TAGS if TAGS is not None else []
+    settings["slytagsplit"] = SLYTAGSPLIT
+    settings["tags"] = TAGS
+
+    settings["explore_datasets"] = SECTION_EXPLORE_CUSTOM_DATASETS
 
     return settings
