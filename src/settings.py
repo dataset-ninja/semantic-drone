cb6a9e9b041c53f88e9b983e0e475df0869b87da
Ars Vladimirov <grokhael@gmail.com>
2023-09-15 17:29:27 +0600

    CLASS2COLOR

--- src/settings.py
+++ src/settings.py
@@ -48,7 +48,32 @@ DOWNLOAD_ORIGINAL_URL: Optional[
 ] = "https://docs.google.com/forms/d/e/1FAIpQLSfhqqdqoB-2c3S8FVq5YsToTExF-EQ4KFeUrYvEuylGSDX0kA/viewform"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
-CLASS2COLOR: Optional[Dict[str, List[str]]] = None
+CLASS2COLOR: Optional[Dict[str, List[str]]] = {
+    "unlabeled": [0, 0, 0],
+    "paved-area": [128, 64, 128],
+    "dirt": [130, 76, 0],
+    "grass": [0, 102, 0],
+    "gravel": [112, 103, 87],
+    "water": [28, 42, 168],
+    "rocks": [48, 41, 30],
+    "pool": [0, 50, 89],
+    "vegetation": [107, 142, 35],
+    "roof": [70, 70, 70],
+    "wall": [102, 102, 156],
+    "window": [254, 228, 12],
+    "door": [254, 148, 12],
+    "fence": [190, 153, 153],
+    "fence-pole": [153, 153, 153],
+    "person": [255, 22, 96],
+    "dog": [102, 51, 0],
+    "car": [9, 143, 150],
+    "bicycle": [119, 11, 32],
+    "tree": [51, 51, 0],
+    "bald-tree": [190, 250, 190],
+    "ar-marker": [112, 150, 146],
+    "obstacle": [2, 135, 115],
+    "conflicting": [255, 0, 0],
+}
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
